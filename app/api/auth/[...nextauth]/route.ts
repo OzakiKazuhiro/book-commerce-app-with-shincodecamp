494c1c72f530538e6ed2cd4d74d60b0c4559e5fa
OzakiKazuhiro <sciencegreen1110@gmail.com>
2024-11-28 10:16:42 +0900

    11/28

---
+++ app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,8 @@
+// /app/api/auth/[...nextauth]/route.ts
+import { nextAuthOptions } from "@/app/lib/next-auth/options";
+
+import NextAuth from "next-auth";
+
+const handler = NextAuth(nextAuthOptions);
+
+export { handler as GET, handler as POST };
